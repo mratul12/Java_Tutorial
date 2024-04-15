1b63e5e1ff47358dd8fe3307275eaa8bd52bd51b
mratul12 <dubeyatuldubey84@gmail.com>
2024-04-16 05:17:09 +0530

    modified

--- Excersice/BankTax.java
+++ Excersice/BankTax.java
@@ -18,8 +18,8 @@ public class BankTax {
             tax = tax + 0.2f * (income - 5.0f);
         }
         else if(income<10.0f){
-            tax = tax + 0.05f * (10.0f - 5.0f);
-            tax = tax + 0.2f * (5.0f - 2.5f);
+            tax = tax + 0.05f * (5.0f - 2.5f);
+            tax = tax + 0.2f * (10.0f - 5.0f);
             tax = tax + 0.3f * (income - 10.0f);
         }
         System.out.println("This is your Anual Taxes: " +tax);
