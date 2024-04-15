6dbcfd13671bf59af7fc72e43e292974ed5dfdf9
mratul12 <dubeyatuldubey84@gmail.com>
2024-04-16 01:12:10 +0530

    Added a program to income tax

---
+++ Excersice/BankTax.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class BankTax {
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+        System.out.println("Enter your income lakh in anual");
+        float tax=0;
+        Scanner sc=new Scanner(System.in);
+        float income=sc.nextFloat();
+        if(income<=2.5f){
+            tax = tax + 0;
+        }
+        else if(income>2.5f && income<=5.0f){
+            tax = tax + 0.05f * (income - 2.5f);
+        }
+        else if(income>5.0f && income <=10.0f){
+            tax = tax + 0.05f * (5.0f - 2.5f);
+            tax = tax + 0.2f * (income - 5.0f);
+        }
+        else if(income<10.0f){
+            tax = tax + 0.05f * (10.0f - 5.0f);
+            tax = tax + 0.2f * (5.0f - 2.5f);
+            tax = tax + 0.3f * (income - 10.0f);
+        }
+        System.out.println("This is your Anual Taxes: " +tax);
+    }
+}
