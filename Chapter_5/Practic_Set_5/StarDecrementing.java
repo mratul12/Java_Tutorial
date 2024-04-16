23e882d97e55f1e074c36c4a7cb46135a66e12bd
mratul12 <dubeyatuldubey84@gmail.com>
2024-04-16 16:25:28 +0530

    Chapter-5 Practice-Set 1

---
+++ Chapter_5/Practic_Set_5/StarDecrementing.java
@@ -0,0 +1,13 @@
+package Chapter_5.Practic_Set_5;
+
+public class StarDecrementing {
+    public static void main(String[] args) {
+    
+      for(int i=5; i>0; i--){
+        for(int j=0; j<i; j++){
+            System.out.print("*");
+        }
+        System.out.print("\n");
+      }
+    }
+}
